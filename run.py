1e6d16715dfd11d285296db1be88841f9a1f1be5
Mike Lagomarsino <mike.lagoma@gmail.com>
2016-06-22 22:03:13 -0700

    test gitignore

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ from flask_script import Server, Manager
 from app import app, models, db
 from app.sms import get_price
 from googlefinance import getQuotes
-#from .models import User, Subscription
 from twilio.rest import TwilioRestClient
 from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 import schedule
@@ -12,30 +11,22 @@ import time
 
 manager = Manager(app)
 server = Server(host="0.0.0.0", port=5400)
-manager.add_command("runserver", server)#Server())
+manager.add_command("runserver", server)
 
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 #@manager.command
 def job():
     users = models.User.query.all()
-    #price = get_price(users[4],"GOOG")
-    print(users)
     for xuser in range(len(users)):
-        print(xuser)
         user = users[xuser]
-        print(user)
         subs = models.Subscription.query.filter_by(subscriber=user).all()
-        print(subs)
         if subs:
             symbols = list()
             for sub in subs:
                 symbols.append(sub.symbol)
             stocks = getQuotes(symbols)
-            print('sending test message')
             prices = ['%s: %s'%(stock['StockSymbol'],stock['LastTradePrice']) for stock in stocks]
-            print(user.phone)
-            print(prices)
             try:
                 sms = client.messages.create(body=', '.join(prices),
                         to=user.phone,
@@ -43,7 +34,7 @@ def job():
             except:
                 print('number not verified, remove from database')
 
-schedule.every(1).minutes.do(job)
+schedule.every(10).minutes.do(job)
 
 @manager.command
 def subs():
